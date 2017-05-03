8dbe30f9531f467b14b2fde66cad7e83998df7d1
Tyler Ches <ttches@gmail.com>
2017-05-02 20:19:24 -0700

    Filters for equal, less, greater show up in console with AND

--- src/containers/ConsoleContainer.js
+++ src/containers/ConsoleContainer.js
@@ -18,20 +18,30 @@ class ConsoleContainer extends Component {
       <div>
         <div>{this.renderTargets()}</div>
         <div>{this.renderTables()}</div>
+        <div>{this.renderFilters()}</div>
       </div>
     )
   }
 
   renderTargets() {
-    return <span><span className='keyword'>
-     SELECT</span> {this.props.selected.targets.join(', ')}</span>
+    return (
+      <span>
+        <span className='keyword'>SELECT </span>
+        {this.props.selected.targets.join(', ')}
+      </span>
+    )
   }
 
   renderTables() {
-    return <span><span className='keyword'>
-     FROM</span> {this.consolidateSelectedTables()}</span>
+    return (
+      <span>
+        <span className='keyword'>FROM </span>
+        {this.consolidateSelectedTables()}
+      </span>
+    )
   }
 
+  //This removes duplicate Tables so they don't appear more than once in FROM
   consolidateSelectedTables() {
     let tables = Array.from(new Set(this.props.selected.targets
       .map((tableRecord) => {
@@ -39,6 +49,57 @@ class ConsoleContainer extends Component {
       return tableRecord
     })));
     return tables.join(', ');
+  }
+
+  renderFilters() {
+    let { equal, lessThan, greaterThan, not, like } = this.props.selected;
+    //Determines whether any filters exist
+    const filterNumber = [equal, lessThan, greaterThan, not, like]
+      .reduce((total, filter) => {
+        return total + Object.keys(filter).length
+      }, 0);
+    //If there are no filters, do not try to add them.
+    if (filterNumber) {
+      const equalLessGreaterArr = ['equal', 'lessThan', 'greaterThan'];
+      let filterOutput =
+        equalLessGreaterArr.map(this.renderEqualLessGreater.bind(this))
+    //gets rid of blank array items
+        .filter((filter) => filter !== '');
+    //flatten array of filters
+      filterOutput = [].concat(...filterOutput);
+      console.log(filterOutput);
+      return (
+        <span>
+          <span className='keyword'>WHERE </span>
+          {filterOutput.map(this.renderAnd)}
+        </span>
+      )
+    }
+  }
+
+  renderAnd(filterValue, i) {
+    if (i === 0) {
+      return (
+        <span key={i}>
+          {filterValue}
+        </span>
+      )
+    }
+    return (
+      <span key={i}>
+        <span className='keyword'> AND </span>
+        {filterValue}
+      </span>
+    )
+  }
+
+  renderEqualLessGreater(filter) {
+    console.log('filter =', filter)
+    const symbols = {equal: '=', lessThan: '<', greaterThan: '>'};
+    console.log(this.props.selected[filter])
+    return Object.keys(this.props.selected[filter]).map((tableRecord) => {
+      return `${tableRecord} ${symbols[filter]} ${this.props.selected[filter][tableRecord]}`
+    })
   }
 
   render() {
